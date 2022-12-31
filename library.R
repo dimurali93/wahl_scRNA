bc92bb16d9f4bdb62436d1d4216898bb29fb28ed
dimurali93 <dimurlai93@github.com>
2022-12-31 00:29:10 -0800

    adding all requied lib/packages

---
+++ library.R
@@ -0,0 +1,33 @@
+# install.packages("BiocManager")
+# devtools::install_github('satijalab/seurat-wrappers')
+# devtools::install_github('cole-trapnell-lab/monocle3')
+# credentials::set_github_pat("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")
+
+library("Seurat")
+library("ggplot2")
+library("ggpubr")
+library("enrichR")
+library("clusterProfiler")
+library("patchwork")
+library("monocle3")
+library("SeuratWrappers")
+library("dplyr")
+library("cowplot")
+library("reshape2")
+library("RColorBrewer")
+library("tibble")
+library("pheatmap")
+library("tidyr")
+library("viper")
+library("stringr")
+library("decoupleR")
+library("OmnipathR")
+
+
+
+# this might not work
+# install.packages("remotes")
+# install_github("Displayr/flipChartBasics",force = TRUE)
+# library("flipChartBasics")
+
+
